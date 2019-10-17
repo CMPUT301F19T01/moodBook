990536da74268463655c426d65bda80cdf22580f
Kathleen Pescador <kpescado@ualberta.ca>
2019-10-17 11:49:44 -0600

    add activity

--- moodBookProject/app/src/main/java/com/example/moodbook/AddMoodActivity.java
+++
@@ -1,5 +0,0 @@
-package com.example.moodbook;
-
-public class AddMoodActivity {
-
-}
---
+++ moodBookProject/app/src/main/java/com/example/moodbook/CreateMoodActivity.java
@@ -0,0 +1,14 @@
+package com.example.moodbook;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CreateMoodActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_mood);
+    }
+}
