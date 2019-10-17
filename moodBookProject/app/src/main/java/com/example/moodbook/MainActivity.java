8013f1628e1f8e2ab5cf49b8da7d268ad81cf95c
Abunie <amandaabuni_gaiya@yahoo.com>
2019-10-17 16:42:13 -0600

    Just changed the linked the button to the createMood Page :)

--- moodBookProject/app/src/main/java/com/example/moodbook/MainActivity.java
+++ moodBookProject/app/src/main/java/com/example/moodbook/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.moodbook;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
@@ -20,10 +21,12 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
 import android.view.Menu;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
     private AppBarConfiguration mAppBarConfiguration;
+    private FloatingActionButton add_mood_button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,14 +34,21 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-        FloatingActionButton fab = findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
+        add_mood_button =findViewById(R.id.fab);
+        add_mood_button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+               addMood();
             }
         });
+//        FloatingActionButton fab = findViewById(R.id.fab);
+//        fab.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+//                        .setAction("Action", null).show();
+//            }
+//        });
         DrawerLayout drawer = findViewById(R.id.drawer_layout);
         NavigationView navigationView = findViewById(R.id.nav_view);
         // Passing each menu ID as a set of Ids because each
@@ -65,5 +75,11 @@ public class MainActivity extends AppCompatActivity {
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
         return NavigationUI.navigateUp(navController, mAppBarConfiguration)
                 || super.onSupportNavigateUp();
+    }
+    /** Called when the user taps the Send button */
+    public void addMood() {
+        // Do something in response to button
+        Intent intent = new Intent(this, CreateMoodActivity.class);
+        startActivity(intent);
     }
 }
