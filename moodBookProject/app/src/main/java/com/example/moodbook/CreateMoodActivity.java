2f5104b1bc5e6746445ecafbf26fb6e63b33a521
Kathleen Pescador <kpescado@ualberta.ca>
2019-10-18 01:31:20 -0600

    check again

--- moodBookProject/app/src/main/java/com/example/moodbook/CreateMoodActivity.java
+++ moodBookProject/app/src/main/java/com/example/moodbook/CreateMoodActivity.java
@@ -7,10 +7,14 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 
 public class CreateMoodActivity extends AppCompatActivity {
 
     Button pick_mood;
+    Button add_photo;
+    ImageView view_photo;
+    private static final int REQUEST_IMAGE = 101;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,5 +31,13 @@ public class CreateMoodActivity extends AppCompatActivity {
             }
         });
 
+
+        add_photo = findViewById(R.id.pick_mood_photo);
+        view_photo = findViewById(R.id.fill_mood_photo);
+    }
+
+
+    public void setImage(View view){
+        
     }
 }
