3928d6f932c17fb14092afde129f2878fbbb9e0e
Kathleen Pescador <kpescado@ualberta.ca>
2019-10-18 01:43:28 -0600

    photo capture

--- moodBookProject/app/src/main/java/com/example/moodbook/CreateMoodActivity.java
+++ moodBookProject/app/src/main/java/com/example/moodbook/CreateMoodActivity.java
@@ -1,10 +1,13 @@
 package com.example.moodbook;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.FragmentManager;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
@@ -34,10 +37,28 @@ public class CreateMoodActivity extends AppCompatActivity {
 
         add_photo = findViewById(R.id.pick_mood_photo);
         view_photo = findViewById(R.id.fill_mood_photo);
+        add_photo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                setImage(view);
+            }
+        });
     }
 
 
     public void setImage(View view){
-        
+        Intent imageIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        if (imageIntent.resolveActivity(getPackageManager()) != null) {
+            startActivityForResult(imageIntent, REQUEST_IMAGE);
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        if (requestCode==REQUEST_IMAGE && resultCode==RESULT_OK){
+            Bundle extras = data.getExtras();
+            Bitmap imageBitmap = (Bitmap) extras.get("data");
+            view_photo.setImageBitmap(imageBitmap);
+        }
     }
 }
