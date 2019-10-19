5213703884a51cb0cfd5dbcff121d0c8e8a1a12c
Abunie <amandaabuni_gaiya@yahoo.com>
2019-10-19 00:15:25 -0600

    Select Mood Fragment

--- moodBookProject/app/src/main/java/com/example/moodbook/SelectMoodStateFragment.java
+++ moodBookProject/app/src/main/java/com/example/moodbook/SelectMoodStateFragment.java
@@ -9,8 +9,10 @@ import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ListView;
+import android.widget.RelativeLayout;
 import android.widget.SearchView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.Nullable;
 import androidx.fragment.app.DialogFragment;
@@ -20,27 +22,12 @@ public class SelectMoodStateFragment extends DialogFragment {
 
     Button select_button;
     Button cancel_button;
-    TextView happy;
-    TextView sad;
-    TextView afraid;
-    TextView angry;
-//    ListView moodView;
+    RelativeLayout happy;
+    RelativeLayout sad;
+    RelativeLayout afraid;
+    RelativeLayout angry;
+    String selectedItem = "";
     SearchView sv;
-
-//    ArrayAdapter<String> statesadapter;
-//    ArrayAdapter<Integer> emojiadapter;
-//    String[] MoodStates = {"Happy", "Sad", "Afraid", "Angry"};
-//    int[] MoodEmoji = {R.drawable.happy,R.drawable.sad, R.drawable.afraid, R.drawable.angry };
-
-//    @Override
-//    public Dialog onCreateDialog(@Nullable Bundle savedInstanceState){
-//        View view = LayoutInflater.from(getActivity()).inflate(R.layout.select_mood_state,null);
-//        ListView moodView;
-//        moodView = view.findViewById(R.id.moodListView);
-//
-//
-//
-//    }
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState){
         View rootView = inflater.inflate(R.layout.select_mood_state,null);
@@ -48,29 +35,66 @@ public class SelectMoodStateFragment extends DialogFragment {
         //Set Title Dialog Title
         getDialog().setTitle("Pick a Mood");
 
-        //Button, Listview, SearchView Initializations
-//        moodView = rootView.findViewById(R.id.moodListView);
+
+        happy = rootView.findViewById(R.id.happy_layout);
+        sad = rootView.findViewById(R.id.sad_layout);
+        afraid = rootView.findViewById(R.id.afraid_layout);
+        angry = rootView.findViewById(R.id.angry_layout);
         select_button = rootView.findViewById(R.id.select);
         cancel_button = rootView.findViewById(R.id.cancel);
-//        sv = rootView.findViewById(R.id.search_my_mood);
 
-        //Create and Set Adapter to Listview
-//        statesadapter = new ArrayAdapter<String>(getActivity(),android.R.layout.simple_list_item_1,MoodStates);
-////        moodView.setAdapter(statesadapter);
+        afraid.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
 
-//        sv.setQueryHint("Search...");
-//        sv.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
-//            @Override
-//            public boolean onQueryTextSubmit(String s) {
-//                return false;
-//            }
-//
-//            @Override
-//            public boolean onQueryTextChange(String s) {
-//                statesadapter.getFilter().filter(s);
-//                return false;
-//            }
-//        });
+                Toast.makeText(getContext(), "You have Selected Afraid", Toast.LENGTH_LONG).show();
+                selectedItem = "afraid";
+            }
+        });
+        angry.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Toast.makeText(getContext(), "You have Selected Angry", Toast.LENGTH_LONG).show();
+                selectedItem = "angry";
+            }
+        });
+
+        happy.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Toast.makeText(getContext(), "You have Selected Happy", Toast.LENGTH_LONG).show();
+                selectedItem = "happy";
+            }
+        });
+
+        sad.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Toast.makeText(getContext(), " You have Selected Sad", Toast.LENGTH_LONG).show();
+                selectedItem = "sad";
+            }
+        });
+        select_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(selectedItem == ""){
+                    Toast.makeText(getContext(), "You have not selected anything", Toast.LENGTH_LONG).show();
+                }else if(selectedItem =="happy"){
+                    Toast.makeText(getContext(), "Mood : Happy", Toast.LENGTH_LONG).show();
+                }else if(selectedItem =="sad"){
+                    Toast.makeText(getContext(), "Mood : Sad", Toast.LENGTH_LONG).show();
+                }else if(selectedItem =="angry"){
+                    Toast.makeText(getContext(), "Mood : Angry", Toast.LENGTH_LONG).show();
+                }else if(selectedItem =="afraid"){
+                    Toast.makeText(getContext(), "Mood : Afraid", Toast.LENGTH_LONG).show();
+                }else{
+                    Toast.makeText(getContext(), "An Error Occurred", Toast.LENGTH_LONG).show();
+                }
+            }
+        });
 
         cancel_button.setOnClickListener(new View.OnClickListener() {
             @Override
