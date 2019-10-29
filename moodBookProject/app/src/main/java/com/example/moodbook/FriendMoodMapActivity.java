cb735c633005ef33b6b552b5c4542374962624f5
neilZon <neilcviloria@gmail.com>
2019-10-29 16:16:17 -0600

    working drawing, needs integration with Firebase

--- moodBookProject/app/src/main/java/com/example/moodbook/FriendMoodMapActivity.java
+++ moodBookProject/app/src/main/java/com/example/moodbook/FriendMoodMapActivity.java
@@ -1,14 +1,10 @@
 package com.example.moodbook;
 
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.FragmentActivity;
 
 import android.graphics.Bitmap;
 import android.graphics.drawable.BitmapDrawable;
 import android.os.Bundle;
-import android.util.Log;
-import android.widget.Toast;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -18,17 +14,12 @@ import com.google.android.gms.maps.model.BitmapDescriptor;
 import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.Task;
 import com.google.firebase.firestore.FirebaseFirestore;
-import com.google.firebase.firestore.QueryDocumentSnapshot;
-import com.google.firebase.firestore.QuerySnapshot;
 
 import java.util.ArrayList;
 
-public class MoodMapActivity extends AppCompatActivity implements OnMapReadyCallback, DBUpdate {
+public class FriendMoodMapActivity extends FragmentActivity implements OnMapReadyCallback, DBUpdate {
 
-    ///// Member Variables /////
     private GoogleMap moodMap;
     private ArrayList<Mood> moodDataList;
     private FirebaseFirestore db;
@@ -36,7 +27,7 @@ public class MoodMapActivity extends AppCompatActivity implements OnMapReadyCall
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_mood_map);
+        setContentView(R.layout.activity_friend_mood_map);
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
@@ -53,60 +44,18 @@ public class MoodMapActivity extends AppCompatActivity implements OnMapReadyCall
      * it inside the SupportMapFragment. This method will only be triggered once the user has
      * installed Google Play services and returned to the app.
      */
-
-
     @Override
     public void onMapReady(GoogleMap googleMap) {
-        // initialize map
         moodMap = googleMap;
 
-        // connect to db
-        db = FirebaseFirestore.getInstance();
-
-        // test data
-        ArrayList<Mood> moodTESTDATA= new ArrayList<>();
-
-        LatLng loc1 = new LatLng(60.03547, -123.75790);
-        LatLng loc2 = new LatLng(24.26711, 125.54427);
-        LatLng loc3 = new LatLng(3.28003, 107.63163);
-        LatLng loc4 = new LatLng(53.28003, -134.63163);
-
-        Mood moodAfraid = new Mood("Afraid", loc1);
-        Mood moodAngry = new Mood("Angry", loc2);
-        Mood moodHappy = new Mood("Happy", loc3);
-        Mood moodSad = new Mood("Sad", loc4);
-
-        moodTESTDATA.add(moodAfraid);
-        moodTESTDATA.add(moodAngry);
-        moodTESTDATA.add(moodHappy);
-        moodTESTDATA.add(moodSad);
-
-        drawMoodMarkers(moodTESTDATA);
-
-        updateList(db);
-
+        // Add a marker in Sydney and move the camera
+        LatLng sydney = new LatLng(-34, 151);
+        moodMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
+        moodMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
     }
 
-    // update users mood list
     @Override
     public void updateList(FirebaseFirestore db) {
-        // todo: change document path to users token id
-        db.collection("USERS").document("3UqSQpyJRNTiH3TALB64mq0vymy2")
-                .collection("MOODS")
-                .get()
-                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
-                    @Override
-                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
-                        if (task.isSuccessful()) {
-                            for (QueryDocumentSnapshot document : task.getResult()) {
-                                Log.i("uwu", document.getId() + " => " + document.getData());
-                                // todo: get the moods and put them in the arrayList
-                            }
-                        } else {
-                            Log.w("uwu", "Error getting documents.", task.getException());
-                        }
-                    }
-                });
 
     }
 
@@ -129,5 +78,6 @@ public class MoodMapActivity extends AppCompatActivity implements OnMapReadyCall
             moodMap.addMarker(new MarkerOptions().position(emotionLatLng).icon(bitmapDescriptor));
         }
     }
+
 
 }
