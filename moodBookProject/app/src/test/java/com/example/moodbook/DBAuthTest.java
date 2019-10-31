60ca8cf7d1437da3f6b8a8d0400fccfa34da001e
Tyler Elston <24640479+tylerelston@users.noreply.github.com>
2019-10-31 14:39:28 -0600

    mockito

--- moodBookProject/app/src/test/java/com/example/moodbook/DBAuthTest.java
+++ moodBookProject/app/src/test/java/com/example/moodbook/DBAuthTest.java
@@ -2,21 +2,29 @@ package com.example.moodbook;
 
 import com.example.moodbook.ui.login.DBAuth;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.FirebaseFirestore;
 
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 public class DBAuthTest {
 
     private DBAuth mockDBAuth(){
-        FirebaseAuth mAuth = FirebaseAuth.getInstance();
+        //FirebaseAuth mAuth = FirebaseAuth.getInstance();
+        FirebaseAuth mAuth = Mockito.mock(FirebaseAuth.class);
         return new DBAuth(mAuth);
     }
 
+
+
     @Test
     void testVerifyEmail(){
-        DBAuth dbAuth = mockDBAuth();
+        //DBAuth dbAuth = mockDBAuth();
+        FirebaseAuth mAuth = Mockito.mock(FirebaseAuth.class);
+        DBAuth dbAuth = new DBAuth(mAuth);
         // Test valid email
         String email = "testemail@gmail.com";
         Boolean result = dbAuth.verifyEmail(email);
