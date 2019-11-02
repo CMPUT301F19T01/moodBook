bf39d0c0dfc51dea251b617ae2fed8cf57ff8bea
Tyler Elston <24640479+tylerelston@users.noreply.github.com>
2019-11-02 17:33:24 -0600

    layout changes to fragments

--- moodBookProject/app/src/main/java/com/example/moodbook/MainActivity.java
+++ moodBookProject/app/src/main/java/com/example/moodbook/MainActivity.java
@@ -4,13 +4,13 @@ package com.example.moodbook;
 import android.content.Intent;
 import android.os.Bundle;
 
+import com.example.moodbook.ui.Request.RequestFragment;
 import com.example.moodbook.ui.addFriends.addFriendFragment;
 import com.example.moodbook.ui.friendMood.friendMoodFragment;
 import com.example.moodbook.ui.home.HomeFragment;
 import com.example.moodbook.ui.login.LoginActivity;
 import com.example.moodbook.ui.myFriendMoodMap.myFriendMoodMapFragment;
 import com.example.moodbook.ui.myMoodMap.myMoodMapFragment;
-import com.example.moodbook.ui.Request.RequestFragment;
 
 import android.view.MenuItem;
 
@@ -79,36 +79,6 @@ public class MainActivity extends AppCompatActivity   {
                 }
         );
 
-
-        /* Mood History */
-        // Add mood: when floating add button is clicked, start add activity to add a new mood item
-        /*FloatingActionButton add_mood_button = findViewById(R.id.mood_history_add_button);
-        add_mood_button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                System.out.println("click");
-                Intent intent = new Intent(MainActivity.this, CreateMoodActivity.class);
-                startActivity(intent);
-            }
-        });
-        // Set up ListView
-        moodListView = findViewById(R.id.mood_history_listView);
-        moodAdapter = new CustomAdapter(this, new ArrayList<Mood>());
-        // test adding
-        Mood testItem = new Mood(null, null, "happy");
-        moodAdapter.add(testItem);
-        moodListView.setAdapter(moodAdapter);
-        // Edit mood: When a mood item is clicked, start edit activity to edit the selected mood
-        moodListView.setOnItemClickListener(new AdapterView.OnItemClickListener(){
-            @Override
-            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
-                // get the selected mood
-                Mood selectedMood = (Mood)adapterView.getItemAtPosition(i);
-                System.out.println("Selected: "+selectedMood.getEmotionText());
-                // TODO
-            }
-        });*/
-
     }
 
 
@@ -120,15 +90,6 @@ public class MainActivity extends AppCompatActivity   {
         return true;
     }
 
-//    @Override
-//    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
-//         super.onOptionsItemSelected(item);
-//
-//         if (item.getItemId() == R.id.action_settings){
-//            logout();
-//         }
-//         return true;
-//    }
 
     @Override
     public boolean onSupportNavigateUp() {
@@ -139,49 +100,27 @@ public class MainActivity extends AppCompatActivity   {
 
 
     public void selectDrawerItem(MenuItem menuItem, DrawerLayout drawer){
-        Fragment fragment = new Fragment();
+        Fragment fragment;
         Class fragmentClass;
         switch(menuItem.getItemId()){
             case R.id.nav_FriendMood:
                 fragmentClass = friendMoodFragment.class;
-                Toast.makeText(MainActivity.this, " friends Mood clicked",
-                        Toast.LENGTH_SHORT).show();
                 break;
             case R.id.nav_addFriendsMood:
-                fragmentClass = addFriendFragment.class;
-                Toast.makeText(MainActivity.this, "add friends clicked",
-                        Toast.LENGTH_SHORT).show();
-                break;
-
-            case R.id.nav_myRequests:
-//                Toast.makeText(MainActivity.this, "Request clicked",
-//                        Toast.LENGTH_SHORT).show();
-//                Intent intent = getIntent();
-//                startActivity(new Intent(this, RequestFragment.class));
                 fragmentClass = RequestFragment.class;
-
-                Toast.makeText(MainActivity.this, "hello",
-                        Toast.LENGTH_SHORT).show();
-
-//                Toast.makeText(MainActivity.this, "Request here",
-//                        Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.nav_myRequests:
+                fragmentClass = addFriendFragment.class;
                 break;
             case R.id.nav_myMoodMap:
                 fragmentClass = myMoodMapFragment.class;
-                Toast.makeText(MainActivity.this, "My mood map clicked",
-                        Toast.LENGTH_SHORT).show();
                 break;
             case R.id.nav_myFriendMoodMap:
                 fragmentClass = myFriendMoodMapFragment.class;
-                Toast.makeText(MainActivity.this, " friend mood map clicked",
-                        Toast.LENGTH_SHORT).show();
                 break;
             case R.id.nav_logout:
-                 fragmentClass = null;
-
+                fragmentClass = null;
                 logout();
-                Toast.makeText(MainActivity.this, " log out",
-                        Toast.LENGTH_SHORT).show();
                 break;
             default:
                 fragmentClass = HomeFragment.class;
@@ -199,8 +138,6 @@ public class MainActivity extends AppCompatActivity   {
         } catch (Exception e) {
             e.printStackTrace();
         }
-
-
 
     }
 
