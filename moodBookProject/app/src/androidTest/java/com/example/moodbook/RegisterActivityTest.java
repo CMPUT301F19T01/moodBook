19c204419659deb4e386660cbe10228de1d6ad88
Tyler Elston <24640479+tylerelston@users.noreply.github.com>
2019-11-03 11:36:10 -0700

    register activity intent tests

--- moodBookProject/app/src/androidTest/java/com/example/moodbook/RegisterActivityTest.java
+++ moodBookProject/app/src/androidTest/java/com/example/moodbook/RegisterActivityTest.java
@@ -1,5 +1,6 @@
 package com.example.moodbook;
 
+import android.content.Intent;
 import android.widget.EditText;
 
 import androidx.test.platform.app.InstrumentationRegistry;
@@ -31,76 +32,109 @@ public class RegisterActivityTest {
         if (solo.searchText("Mood History")){
             solo.clickOnImageButton(0);
             solo.clickOnText("Logout");
+            solo.sleep(3000);
+            solo.clickOnText("register");
+            solo.sleep(3000);
         }
-        solo.sleep(3000);
     }
 
+//    /**
+//     * Tests register
+//     */
+//    //TODO
+//    @Test
+//    public void registerSucceed(){
+//        solo.enterText((EditText) solo.getView(R.id.email), "test@test.com");
+//        solo.enterText((EditText) solo.getView(R.id.password), "testtest");
+//        solo.clickOnButton("login");
+//        solo.sleep(5000); // wait for activity to change
+//
+//        solo.assertCurrentActivity("Wrong activity", MainActivity.class);
+//
+//    }
+
     /**
-     * Tests login with test@test.com and password testtest for successful login
+     * Tests register for a failed register due to email
      */
     @Test
-    public void loginSucceed(){
-        solo.enterText((EditText) solo.getView(R.id.email), "test@test.com");
-        solo.enterText((EditText) solo.getView(R.id.password), "testtest");
-        solo.clickOnButton("login");
-        solo.sleep(5000); // wait for activity to change
-
-        solo.assertCurrentActivity("Wrong activity", MainActivity.class);
-
-    }
-
-    /**
-     * Tests login for a failed login due to email
-     */
-    @Test
-    public void loginFailEmail(){
+    public void registerFailEmail(){
         // invalid email
-        solo.enterText((EditText) solo.getView(R.id.email), "fail@test.com");
+        solo.enterText((EditText) solo.getView(R.id.email), "fail");
         solo.enterText((EditText) solo.getView(R.id.password), "testtest");
-        solo.clickOnButton("login");
-
-        assertTrue(solo.waitForText("Authentication failed"));
-
-    }
-    /**
-     * Tests login for a failed login due to password
-     */
-    @Test
-    public void loginFailPassword(){
-        // invalid password
-        solo.enterText((EditText) solo.getView(R.id.email), "test@test.com");
-        solo.enterText((EditText) solo.getView(R.id.password), "failfail");
-        solo.clickOnButton("login");
-
-        assertTrue(solo.waitForText("Authentication failed"));
-
-    }
-
-    /**
-     * Tests login for a failed login with empty email
-     */
-    @Test
-    public void loginFailEmptyEmail(){
-        // empty email
-        solo.enterText((EditText) solo.getView(R.id.email), "");
-        solo.enterText((EditText) solo.getView(R.id.password), "testtest");
-        solo.clickOnButton("login");
+        solo.enterText((EditText) solo.getView(R.id.username), "unusedtestuser");
+        solo.clickOnButton("register");
 
         assertTrue(solo.waitForText("Incorrect email format"));
-
     }
 
     /**
-     * Tests login for a failed login with empty password
+     * Tests register for a failed register due to password
      */
     @Test
-    public void loginFailEmptyPassword(){
-        // empty password
-        solo.enterText((EditText) solo.getView(R.id.email), "test@test.com");
-        solo.enterText((EditText) solo.getView(R.id.password), "");
-        solo.clickOnButton("login");
+    public void registerFailPassword(){
+        // invalid email
+        solo.enterText((EditText) solo.getView(R.id.email), "test@gmail.com");
+        solo.enterText((EditText) solo.getView(R.id.password), "fail");
+        solo.enterText((EditText) solo.getView(R.id.username), "unusedtestuser");
+        solo.clickOnButton("register");
 
         assertTrue(solo.waitForText("Password must be"));
-
     }
+
+    /**
+     * Tests register for a failed register due to username taken
+     */
+    @Test
+    public void registerFailUsername(){
+        // invalid email
+        solo.enterText((EditText) solo.getView(R.id.email), "test@gmail.com");
+        solo.enterText((EditText) solo.getView(R.id.password), "testtest");
+        solo.enterText((EditText) solo.getView(R.id.username), "test");
+        solo.clickOnButton("register");
+
+        assertTrue(solo.waitForText("Username in use"));
+    }
+
+    /**
+     * Tests register for a failed register due to empty email
+     */
+    @Test
+    public void registerFailEmptyEmail(){
+        // invalid email
+        solo.enterText((EditText) solo.getView(R.id.email), "");
+        solo.enterText((EditText) solo.getView(R.id.password), "testtest");
+        solo.enterText((EditText) solo.getView(R.id.username), "unusedtestuser");
+        solo.clickOnButton("register");
+
+        assertTrue(solo.waitForText("Incorrect email format"));
+    }
+
+    /**
+     * Tests register for a failed register due to empty username
+     */
+    @Test
+    public void registerFailEmptyUsername(){
+        // invalid email
+        solo.enterText((EditText) solo.getView(R.id.email), "test@gmail.com");
+        solo.enterText((EditText) solo.getView(R.id.password), "testtest");
+        solo.enterText((EditText) solo.getView(R.id.username), "");
+        solo.clickOnButton("register");
+
+        assertTrue(solo.waitForText("Username in use"));
+    }
+
+    /**
+     * Tests register for a failed register due to empty password
+     */
+    @Test
+    public void registerFailEmptyPassword(){
+        // invalid email
+        solo.enterText((EditText) solo.getView(R.id.email), "test@gmail.com");
+        solo.enterText((EditText) solo.getView(R.id.password), "");
+        solo.enterText((EditText) solo.getView(R.id.username), "unusedtestuser");
+        solo.clickOnButton("register");
+
+        assertTrue(solo.waitForText("Password must be"));
+    }
+
 }
