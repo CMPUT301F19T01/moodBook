be0ddcfeb2ed26dddf81c3cacb46e745d92c3893
Tyler Elston <24640479+tylerelston@users.noreply.github.com>
2019-11-03 10:42:03 -0700

    request fragment intent testing

--- moodBookProject/app/src/androidTest/java/com/example/moodbook/RequestFragmentTest.java
+++ moodBookProject/app/src/androidTest/java/com/example/moodbook/RequestFragmentTest.java
@@ -44,18 +44,48 @@ public class RequestFragmentTest {
     }
 
     /**
-     *
+     * Test for successful add request
      */
     // https://stackoverflow.com/questions/16596418/how-to-handle-exceptions-in-junit  - Eric Jablow    used to ignore failed login method
-    @Test(expected = junit.framework.AssertionFailedError.class) // ignore failed login attempt (if already logged in)
+    //@Test(expected = junit.framework.AssertionFailedError.class) // ignore failed login attempt (if already logged in)
+    @Test
     public void sendRequest(){
-        login();
+        //login();
         // navigate to request sending
-        solo.clickOnActionBarHomeButton();
-        solo.clickOnMenuItem("Add Friends");
+        solo.clickOnImageButton(0);
+        solo.clickOnText("Add Friends");
 
-        solo.enterText((EditText) solo.getView(R.id.usernameEditText), "JIM");
+        solo.enterText((EditText) solo.getView(R.id.usernameEditText), "tyler");
         solo.clickOnButton("Send Request");
         assertTrue(solo.waitForText("Sent request"));
+    }
+
+    /**
+     * Test for unsuccessful add request
+     */
+    @Test
+    public void sendRequestInvalid(){
+        //login();
+        // navigate to request sending
+        solo.clickOnImageButton(0);
+        solo.clickOnText("Add Friends");
+
+        solo.enterText((EditText) solo.getView(R.id.usernameEditText), "2j2ieh2@@@dwed33");
+        solo.clickOnButton("Send Request");
+        assertTrue(solo.waitForText("User does not exist"));
+    }
+
+    /**
+     * Test for empty add request
+     */
+    @Test
+    public void sendRequestEmpty(){
+        //login();
+        // navigate to request sending
+        solo.clickOnImageButton(0);
+        solo.clickOnText("Add Friends");
+
+        solo.clickOnButton("Send Request");
+        assertTrue(solo.waitForText("User does not exist"));
     }
 }
