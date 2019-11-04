d620948c6aba63b72713fc69b71aea02f87c2523
Tyler Elston <24640479+tylerelston@users.noreply.github.com>
2019-11-04 14:52:40 -0700

    various import fixes

--- moodBookProject/app/src/main/java/com/example/moodbook/ui/myFriendMoodMap/myFriendMoodMapFragment.java
+++ moodBookProject/app/src/main/java/com/example/moodbook/ui/myFriendMoodMap/myFriendMoodMapFragment.java
@@ -16,6 +16,7 @@ import com.google.firebase.firestore.FirebaseFirestore;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
 
--- moodBookProject/app/src/main/java/com/example/moodbook/ui/myMoodMap/myMoodMapFragment.java
+++ moodBookProject/app/src/main/java/com/example/moodbook/ui/myMoodMap/myMoodMapFragment.java
@@ -32,6 +32,7 @@ import com.google.firebase.firestore.QuerySnapshot;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
 
