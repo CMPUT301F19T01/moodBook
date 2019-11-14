065064c83f95b2968c603b304192babd1971bfbf
Abunie <amandaabuni_gaiya@yahoo.com>
2019-11-14 00:22:31 -0700

    Profile picture display

--- moodBookProject/app/src/main/java/com/example/moodbook/ui/login/RegisterActivity.java
+++ moodBookProject/app/src/main/java/com/example/moodbook/ui/login/RegisterActivity.java
@@ -1,20 +1,27 @@
 package com.example.moodbook.ui.login;
 
+import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.content.Intent;
+import android.graphics.Bitmap;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.example.moodbook.EditMoodActivity;
+import com.example.moodbook.MoodEditor;
 import com.example.moodbook.R;
 import com.example.moodbook.data.UsernameList;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
@@ -25,7 +32,7 @@ import com.google.firebase.firestore.FirebaseFirestore;
  * Citation: https://firebase.google.com/docs/auth/android/manage-users?authuser=0
  */
 
-public class RegisterActivity extends AppCompatActivity {
+public class RegisterActivity extends AppCompatActivity implements ProfileEditor.ProfilePicInterface {
 
     private FirebaseAuth mAuth;
 
@@ -37,6 +44,9 @@ public class RegisterActivity extends AppCompatActivity {
     protected EditText email;
     private EditText username;
     protected EditText password;
+    private FloatingActionButton edit_profile_pic;
+    private ImageView profile_pic;
+    private Bitmap bitImage;
 
     private static final String TAG = "EmailPassword";
 
@@ -55,6 +65,8 @@ public class RegisterActivity extends AppCompatActivity {
         email = findViewById(R.id.email);
         username = findViewById(R.id.username);
         password = findViewById(R.id.password);
+        edit_profile_pic = findViewById(R.id.edit_profile_pic_button);
+        changeProfilePic();
 
         // Register is not not modularized because FireBase calls are asynchronous. Since they are asynchronous, we can't depend on results returned from methods until the onCompleteListener knows that the task is finished
         // REGISTER button
@@ -105,6 +117,41 @@ public class RegisterActivity extends AppCompatActivity {
 
 
     }
+    @SuppressLint("MissingSuperCall")
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        ProfileEditor.getImageResult(requestCode, resultCode, data, profile_pic, this);
+    }
+    /**
+     * This allows a user to edit a mood image.
+     */
+    private void changeProfilePic() {
+        profile_pic = findViewById(R.id.profile_pic);
 
+        edit_profile_pic.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                ProfileEditor.setImage(RegisterActivity.this);
+            }
+        });
+    }
+
+    /**
+     * This is a method inherited from the MoodEditor Interface that sets a value for a bitmap Image
+     * @param bitImage
+     *  This is a bitmap image
+     * This override MoodEditor.MoodInterface setMoodReasonPhoto(),
+     * and is setter for bitImage
+     * @param bitImage
+     *  This is current reason photo of mood event
+     */
+    @Override
+    public void setProfilePic(Bitmap bitImage) {
+        this.bitImage = bitImage;
+    }
 
 }
+
+
+
