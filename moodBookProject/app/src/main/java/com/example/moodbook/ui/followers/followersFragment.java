b744cb54226e5a00f1708892edbac7bbc7ced177
Kathleen Pescador <kpescado@ualberta.ca>
2019-11-23 02:03:40 -0700

    get follower item from listview

--- moodBookProject/app/src/main/java/com/example/moodbook/ui/followers/followersFragment.java
+++ moodBookProject/app/src/main/java/com/example/moodbook/ui/followers/followersFragment.java
@@ -1,14 +1,19 @@
 package com.example.moodbook.ui.followers;
 
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 
 import com.example.moodbook.DBFriend;
+import com.example.moodbook.MoodbookUser;
 import com.example.moodbook.PageFragment;
 import com.example.moodbook.R;
 import com.google.firebase.auth.FirebaseAuth;
@@ -35,6 +40,26 @@ public class followersFragment extends PageFragment {
         mAuth = FirebaseAuth.getInstance();
         friendDB = new DBFriend(mAuth, getContext(), TAG);
         friendDB.setFollowersListListener(followersListAdapter);
+
+        followersListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, final View view, final int pos, long id) {
+                final int row = pos;
+                new AlertDialog.Builder(getActivity())
+                        .setTitle("Remove User")
+                        .setMessage("Are you sure you want to remove this user in your follower list?")
+                        .setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialogInterface, int i) {
+                                MoodbookUser selectedUser = (MoodbookUser) followersListView.getItemAtPosition(pos);
+                                String followerUser = selectedUser.getUsername();
+                                Toast.makeText(getContext(), followerUser, Toast.LENGTH_SHORT).show();
+                            }
+                        })
+                        .setNegativeButton("No", null)
+                        .show();
+            }
+        });
 
         return root;
     }
