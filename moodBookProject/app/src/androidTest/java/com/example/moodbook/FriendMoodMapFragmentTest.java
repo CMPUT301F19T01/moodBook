fc4216058443d6439a028407ba6d88e548399b6b
neilZon <neilcviloria@gmail.com>
2019-11-28 15:08:23 -0700

    adding javadocs to FriendMood

--- moodBookProject/app/src/androidTest/java/com/example/moodbook/FriendMoodMapFragmentTest.java
+++ moodBookProject/app/src/androidTest/java/com/example/moodbook/FriendMoodMapFragmentTest.java
@@ -1,4 +1,49 @@
 package com.example.moodbook;
 
+import android.widget.EditText;
+
+import androidx.test.platform.app.InstrumentationRegistry;
+import androidx.test.rule.ActivityTestRule;
+
+import com.example.moodbook.ui.login.LoginActivity;
+import com.robotium.solo.Solo;
+
+import org.junit.Before;
+import org.junit.Rule;
+
 public class FriendMoodMapFragmentTest {
+    private Solo solo;
+
+    @Rule
+    public ActivityTestRule<LoginActivity> rule =
+            new ActivityTestRule<>(LoginActivity.class, true, true);
+
+
+    @Before
+    public void setUp() throws Exception{
+        solo = new Solo(InstrumentationRegistry.getInstrumentation(), rule.getActivity());
+        // logout if logged in
+        if (solo.searchText("Mood History")){
+            solo.clickOnImageButton(0);
+            for (int i = 0; i < 4; i++){
+                solo.sendKey(Solo.DOWN);
+            }
+            solo.clickOnText("Logout");
+            solo.sleep(3000);
+        }
+        // login with test account
+        if (solo.searchText("login")){
+            login();
+        }
+    }
+
+    /**
+     * used in tests to first login to the app
+     */
+    public void login(){
+        solo.enterText((EditText) solo.getView(R.id.email), "test@test.com");
+        solo.enterText((EditText) solo.getView(R.id.password), "testtest");
+        solo.clickOnButton("login");
+    }
+
 }
