82c92242b91669c8ec29cf16cf245c57418bbc21
Kathleen Pescador <kpescado@ualberta.ca>
2019-11-28 18:01:03 -0700

    cleanup and added javadocs for MoodStateAdapter

--- moodBookProject/app/src/main/java/com/example/moodbook/ui/home/MoodStateAdapter.java
+++ moodBookProject/app/src/main/java/com/example/moodbook/ui/home/MoodStateAdapter.java
@@ -18,16 +18,18 @@ import com.example.moodbook.R;
  * This ArrayAdapter is used by CreateMoodActivity & EditMoodActivity for the spinner of mood emotional states
  */
 public class MoodStateAdapter extends ArrayAdapter<String> {
-    Context context;
-    String[] states;
-    int[] images;
+    private Context context;
+    private String[] states;
+    private int[] images;
 
     /**
-     * This constructor is used by CreateMoodActivity & EditMoodActivity to create ArrayAdapter
-     * for mood emotional states
+     * This constructor is used by CreateMoodActivity & EditMoodActivity to create ArrayAdapter for mood emotional states.
      * @param context
+     * This is a context.
      * @param states
+     * This is the states text stored in an array.
      * @param images
+     * This is the states emotion image stored in an array.
      */
     public MoodStateAdapter(@NonNull Context context, String[] states, int[] images) {
         super(context, R.layout.spinner_item,states);
@@ -36,11 +38,29 @@ public class MoodStateAdapter extends ArrayAdapter<String> {
         this.images = images;
     }
 
+    /**
+     * This method checks if state is enabled.
+     * @param position
+     * This is the position of the state in the array list.
+     * @return
+     * This returns true or false if position is not zero.
+     */
     @Override
     public boolean isEnabled(int position) {
         return (position != 0);
     }
 
+    /**
+     * This method gets a View that displays the data in the drop down popup at the specified position in the data set.
+     * @param position
+     * This is the index of the item whose view we want.
+     * @param convertView
+     * The old view to reuse, if possible. If it is not possible to convert this view to display the correct data, this method can create a new view.
+     * @param parent
+     * The parent that this view will eventually be attached to.
+     * @return row
+     * This returns a View corresponding to the data at the specified position.
+     */
     @Override
     public View getDropDownView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
             LayoutInflater inflater = (LayoutInflater)context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
@@ -54,6 +74,17 @@ public class MoodStateAdapter extends ArrayAdapter<String> {
             return row;
     }
 
+    /**
+     * This method gets a View that displays the data at the specified position in the data set.
+     * @param position
+     * The position of the item within the adapter's data set of the item whose view we want.
+     * @param convertView
+     * The old view to reuse, if possible. If it is not possible to convert this view to display the correct data, this method can create a new view.
+     * @param parent
+     * This returns a View corresponding to the data at the specified position.
+     * @return row
+     * 	A View corresponding to the data at the specified position.
+     */
     @NonNull
     @Override
     public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
@@ -67,6 +98,13 @@ public class MoodStateAdapter extends ArrayAdapter<String> {
         return row;
     }
 
+    /**
+     * This method is used to display error message
+     * @param v
+     * This is a view for TextView.
+     * @param s
+     * This is the error message you want to be displayed.
+     */
     public void setError(View v, CharSequence s) {
         TextView name = (TextView) v.findViewById(R.id.mood_text);
         name.setError(s);
