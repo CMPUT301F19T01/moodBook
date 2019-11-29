c53e66fab06f9f68f8443559bf130847aca87258
neilZon <neilcviloria@gmail.com>
2019-11-29 12:38:16 -0700

    friend map testing

--- moodBookProject/app/src/androidTest/java/com/example/moodbook/FriendMoodMapFragmentTest.java
+++ moodBookProject/app/src/androidTest/java/com/example/moodbook/FriendMoodMapFragmentTest.java
@@ -1,7 +1,7 @@
 package com.example.moodbook;
 
-import android.app.Activity;
 import android.widget.EditText;
+import android.widget.TextView;
 
 import androidx.test.platform.app.InstrumentationRegistry;
 import androidx.test.rule.ActivityTestRule;
@@ -10,9 +10,7 @@ import androidx.test.uiautomator.UiObject;
 import androidx.test.uiautomator.UiObjectNotFoundException;
 import androidx.test.uiautomator.UiSelector;
 
-import com.example.moodbook.ui.friendMood.FriendMood;
 import com.example.moodbook.ui.login.LoginActivity;
-import com.example.moodbook.ui.myFriendMoodMap.MyFriendMoodMapFragment;
 import com.google.android.gms.maps.MapView;
 import com.robotium.solo.Solo;
 
@@ -20,8 +18,7 @@ import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 
-import java.util.ArrayList;
-
+import java.util.HashMap;
 
 import static androidx.test.platform.app.InstrumentationRegistry.getInstrumentation;
 import static org.junit.Assert.assertEquals;
@@ -45,18 +42,53 @@ public class FriendMoodMapFragmentTest {
     }
 
     /**
+     * helper method for setting up friends moods
+     * @param solo
+     * @return
+     *  hash map of friends mood attributes
+     */
+    private HashMap<String, String> addFriendMood(Solo solo){
+        // logout if logged in
+        if (solo.searchText("Mood History")){
+            solo.clickOnImageButton(0);
+            for (int i = 0; i < 4; i++){
+                solo.sendKey(Solo.DOWN);
+            }
+            TestHelper.scrollToLogout(solo);
+            solo.sleep(3000);
+        }
+        // login with test account
+        if (solo.searchText("login")){
+            solo.enterText((EditText) solo.getView(R.id.email), "newtest@test.com");
+            solo.enterText((EditText) solo.getView(R.id.password), "testtest");
+            solo.clickOnButton("login");
+        }
+
+        HashMap<String, String> moodHashMap = TestHelper.addMoodComplete(solo);
+
+        TestHelper.setup(solo);
+
+        return moodHashMap;
+
+    }
+
+    /**
      * Test if map is ready and viewing moods on map
      */
     @Test
     public void test() throws UiObjectNotFoundException {
-        // switch to mood map fragment
         solo = new Solo(InstrumentationRegistry.getInstrumentation(), rule.getActivity());
+        HashMap<String, String>  friendMoodAttributes = addFriendMood(solo);
+        String date = friendMoodAttributes.get("date");
+        String time = friendMoodAttributes.get("time");
+
+        // switch to mood map fragment
         solo.clickOnImageButton(0);
         solo.clickOnText("Friend Mood Map");
         solo.sleep(3000);
 
         // find map
-        MapView mapView = (MapView) solo.getView(R.id.mapView);
+        MapView mapView = (MapView) solo.getView(R.id.friendMapView);
 
         // test if map is ready to be used
         assertEquals(  "Expected map view to be ready","MAP READY", mapView.getContentDescription());
@@ -66,9 +98,17 @@ public class FriendMoodMapFragmentTest {
 
         // find marker on map view
         UiDevice device = UiDevice.getInstance(getInstrumentation());
-        UiObject marker = device.findObject(new UiSelector().descriptionContains("2019-11-28" + " " + "17:29:54"));
+        UiObject marker = device.findObject(new UiSelector().descriptionContains(date + " " + time));
 
         marker.click();
+
+        // test emotion text
+        TextView emotionText = (TextView) solo.getView(R.id.view_emotion);
+        assertEquals(emotionText.getText().toString(), friendMoodAttributes.get("emotion"));
+
+        // test dateTime text
+        TextView dateTimeText = (TextView) solo.getView(R.id.view_date_time);
+        assertEquals(dateTimeText.getText().toString(), date + " at " + time);
 
     }
 
